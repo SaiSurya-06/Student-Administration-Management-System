ede37af76f89cc9f1173d63a340fbacaaf122a93
Arjun behara <saiarjun1202@gmail.com>
2022-12-29 15:30:17 +0530

    added css to sdata

--- sdata.php
+++ sdata.php
@@ -7,23 +7,71 @@ $result=mysqli_query($conn,$query);
 
 <!DOCTYPE html>
 <html>
+	<head>
+
 	<title>
 		Student Data
 	</title>
-	<body>
+	<link rel="icon" type="image/x-icon" href="images\svit-logo.png">
+
+	<style>
+		h1{
+
+			font-family:serif;
+			font-size:xx-large;
+			color:green;
+			align:center;
 		
-		<table aligen="center" border="1px" style="width:600px; line-height:40px;">
+
+
+		}
+		th{
+			font-family:bold;
+			color:navy;
+			font-size:large;
+            border:1px solid red;
+
+			
+
+
+		}
+		body{
+			
+			border:green;
+			background-color:white;
+		}
+		
+		td{
+			border:1px solid red;
+		
+		}
+		div{
+			align:center;
+		}
+		img{
+			position: relative;
+			left:650px;
+		}
+		
+		
+		</style>
+</head>
+
+	<body>
+		<img src="images\svit-logo.png">
+		<table   style="width:600px; line-height:40px; border:2px solid red ;align:center;">
 		<tr>
-			<th colspan="4"><h2>Student Record</h2></th>
+			<h1 style="text-align:center">Student Record</h1>
 		</tr>
+	
 		<t>
-			<th>firstName</th>
-			<th>lastName</th>
+			<th>firstname</th>
+			<th>lastname</th>
 			<th>snumber</th>
 			<th>aadharnumber</th>
 			<th>email</th>
-			<th>FatherName</th>
-			<th>MotherName</th>
+			<th>Fathername</th>
+			<th>Mothername</th>
 			<th>Fathernumber</th>
 			<th>Mothernumber</th>
 			<th>Address</th>
@@ -41,13 +89,13 @@ $result=mysqli_query($conn,$query);
 				{
 		?>
 			<tr>
-				<td><?php echo $rows['firstName']; ?></td>
-				<td><?php echo $rows['lastName']; ?></td>
+				<td><?php echo $rows['firstname']; ?></td>
+				<td><?php echo $rows['lastname']; ?></td>
 				<td><?php echo $rows['snumber']; ?></td>
 				<td><?php echo $rows['aadharnumber']; ?></td>
 				<td><?php echo $rows['email']; ?></td>
-				<td><?php echo $rows['FatherName']; ?></td>
-				<td><?php echo $rows['MotherName']; ?></td>
+				<td><?php echo $rows['Fathername']; ?></td>
+				<td><?php echo $rows['Mothername']; ?></td>
 				<td><?php echo $rows['Fathernumber']; ?></td>
 				<td><?php echo $rows['Mothernumber']; ?></td>
 				<td><?php echo $rows['Address']; ?></td>
@@ -65,6 +113,44 @@ $result=mysqli_query($conn,$query);
 		?>
 		</table>	
 
-	</body>
+	<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
 
 </html>
